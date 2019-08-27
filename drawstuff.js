1ce5dd2806e3d9bb326f39d3891a0e83806c95c0
nnbenven <nnbenven@ncsu.edu>
2019-08-27 12:39:49 -0400

    Update drawstuff.js

--- drawstuff.js
+++ drawstuff.js
@@ -79,8 +79,8 @@ function main() {
  
     // Draw a rectangle with pixels
     var c = new Color(255,0,0,255); // the color at the pixel: black opaque
-    for (var x=10; x<90; x++) 
-        for (var y=10; y<50; y++) {
+    for (var x=0; x<50; x++) 
+        for (var y=0; y<50; y++) {
             drawPixel(imagedata,x,y,c);
             // console.log("draw at " +x+ " " +y);
         }
